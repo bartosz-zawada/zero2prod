9bfb098d9d9cd4afd4d77d3fa39c2f35909260ba
Bartosz Zawada <zawada.bartosz@gmail.com>
2025-09-07 20:07:10 -0700

    feat: separate into lib+bin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web::get};
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
