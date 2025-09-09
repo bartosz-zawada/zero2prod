0c20e342d455471e0e140c47b3419cacdeae76e0
Bartosz Zawada <zawada.bartosz@gmail.com>
2025-09-09 16:45:38 -0700

    feat(config): create configuration.ron and read it from main

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+mod configuration;
 mod routes;
 mod startup;
 
+pub use configuration::get_configuration;
 pub use startup::run;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::net::{SocketAddr, TcpListener};
 
-use zero2prod::run;
-
-const PORT: u16 = 8000;
+use zero2prod::{get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], configuration.application_port));
     let listener = TcpListener::bind(addr)?;
     println!("Listening on {addr}");
 
