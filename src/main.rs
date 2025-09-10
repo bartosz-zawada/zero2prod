2260e99948f56580bd5fbfc6148c1f7e63431a64
Bartosz Zawada <zawada.bartosz@gmail.com>
2025-09-09 22:16:30 -0700

    feat(logging): default env_logger setup and actix-web logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::{SocketAddr, TcpListener};
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::{get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let db_pool = PgPool::connect(&configuration.database.connection_string())
         .await
