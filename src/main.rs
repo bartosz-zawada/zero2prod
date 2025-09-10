52df4772f4adc9797d665b26ab649ccd82c23bcf
Bartosz Zawada <zawada.bartosz@gmail.com>
2025-09-10 16:12:19 -0700

    refactor: move logging setup logic into get_subscriber and init_subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
 use std::net::{SocketAddr, TcpListener};
 
 use sqlx::PgPool;
-use tracing::subscriber;
+use tracing::{Subscriber, subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::{get_configuration, run};
 
+/// Compose multiple layer into a `tracing`'s subscriber
+pub fn get_subscriber<S: Into<String>, L: AsRef<str>>(
+    name: S,
+    default_level: L,
+) -> impl Subscriber {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(default_level));
+
+    let formatting_layer = BunyanFormattingLayer::new(name.into(), std::io::stdout);
+
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as a global default to process span data.
+///
+/// It should only be called once!
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set log redirecting to tracing");
+    subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Set up logging
-    LogTracer::init().expect("Failed to set log redirecting to tracing");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod", "info");
+    init_subscriber(subscriber);
 
     // Read config
     let configuration = get_configuration().expect("Failed to read configuration");
