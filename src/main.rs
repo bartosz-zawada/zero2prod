6c95cfa6690eddb95358f90d556e89749f70e261
Bartosz Zawada <zawada.bartosz@gmail.com>
2025-09-10 15:29:10 -0700

    feat(logging): add custom subscriber with bunyan JSON formatting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 use std::net::{SocketAddr, TcpListener};
 
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::subscriber;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::{get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Set up logging
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+
+    // Read config
     let configuration = get_configuration().expect("Failed to read configuration");
     let db_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to the Database");
 
+    // Bind listener
     let addr = SocketAddr::from(([127, 0, 0, 1], configuration.application_port));
     let listener = TcpListener::bind(addr)?;
     println!("Listening on {addr}");
 
+    // Start server
     run(listener, db_pool)?.await
 }
